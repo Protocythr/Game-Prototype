6d9bf2b9d67fd1ac3bf2abb19b74c1b073a2ba24
Wen Chen <wenchen0275@gmail.com>
2026-07-18 19:59:30 -0400

    added basic classes template into the game files

--- python_game_file.py
+++ python_game_file.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -6,6 +7,43 @@ pygame.init()
 screen = pygame.display.set_mode((640,640))
 
 running = True
+
+# this create the base player class with the basic stats that the player will have
+# do not edit this class to create a subclass
+class Player:
+    def __init__():
+        self.health = 0
+        self.str = 0
+        self.vit = 0
+        self.int = 0
+        self.luck = 0
+
+class Vanguard(Player):
+    def __init__():
+        super().__init__()
+        self.health = 20 + random.randint(0, 12)
+        self.str = 5 + random.randint(0, 5)
+        self.vit = 3 + random.randint(0, 3)
+        self.int = 2 + random.randint(0, 2)
+        self.luck = 0 + random.randint(-6, 6)
+
+class Tank(Player):
+    def __init__():
+        super().__init__()
+        self.health = 30 + random.randint(0, 12)
+        self.str = 3 + random.randint(0, 2)
+        self.vit = 5 + random.randint(0, 5)
+        self.int = 2 + random.randint(0, 2)
+        self.luck = 0 + random.randint(-6, 6)
+
+class Support(Player):
+    def __init__():
+        super().__init__()
+        self.health = 15 + random.randint(0, 12)
+        self.str = 3 + random.randint(0, 3)
+        self.vit = 2 + random.randint(0, 2)
+        self.int = 5 + random.randint(0, 5)
+        self.luck = 0 + random.randint(-6, 6)
 
 # game loop
 while running:
