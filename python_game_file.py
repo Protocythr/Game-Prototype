9e56e70b3652f3bba92091b91fdc17bf8741e1d7
Wen Chen <wenchen0275@gmail.com>
2026-07-17 23:57:40 -0400

    added window

--- python_game_file.py
+++ python_game_file.py
@@ -2,4 +2,20 @@ import pygame
 
 pygame.init()
 
+# intialize the screen with a width and height of 640 pixels
 screen = pygame.display.set_mode((640,640))
+
+running = True
+
+# game loop
+while running:
+    screen.fill((255, 255, 255)) # Fill the screen with white color
+    
+    # checking if someone clicked the close button on the window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip() # Update the display/ basically displays the changes made to the screen
+
+pygame.quit()
